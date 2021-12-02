c50a4521a4d5845d6235820185ae80b0d32dd877
Andrew A. Loesel <aaloesel@svsu.edu>
2021-12-02 14:53:16 -0500

    initial commit

---
+++ app/src/main/java/com/example/perfectshot/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.perfectshot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.perfectshot.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void startPost(View view){
+        Intent i = new Intent(this, PostActivity.class);
+        startActivity(i);
+    }
+
+    public void startMap(View view){
+        Intent i = new Intent(this, MapsActivity.class);
+        startActivity(i);
+    }
+
+    public void startLogin(View view){
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
+    }
+
+    public void startSettings(View view){
+        Intent i = new Intent(this, Settings.class);
+        startActivity(i);
+    }
+}
