ab99bfe66cf7b3be7f78a688058ef2789d5383da
Zachery Kangas <zachery.kangas22@gmail.com>
2021-12-09 12:27:20 -0500

    Layouts for Post Activity, Settings Activity, and Post Cardview

--- app/src/main/java/com/example/perfectshot/PostActivity.java
+++ app/src/main/java/com/example/perfectshot/PostActivity.java
@@ -1,14 +1,39 @@
 package com.example.perfectshot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class PostActivity extends AppCompatActivity {
+
+    Button btnPostToMain;
+    Button btnPostToSettings;
+
+    Button btnMakePost;
+    RecyclerView recPosts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        btnPostToMain = findViewById(R.id.btnPostToMain);
+        btnPostToSettings = findViewById(R.id.btnPostToSettings);
+        btnMakePost = findViewById(R.id.btnMakePost);
+        recPosts = findViewById(R.id.recPosts);
+
+        btnPostToMain.setOnClickListener(v->{
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        btnPostToSettings.setOnClickListener(v->{
+            Intent intent = new Intent(this, Settings.class);
+            startActivity(intent);
+        });
+
     }
 }
