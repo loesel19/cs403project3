dcc562f70e991c0f063a4d521034ed728b2fbdc6
Zachary Barkey <gandalfthegreat747@yahoo.com>
2021-12-09 20:59:34 -0500

    Added permission checks to MainActivity.java

--- app/src/main/java/com/example/perfectshot/MainActivity.java
+++ app/src/main/java/com/example/perfectshot/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.perfectshot;
 
+import android.Manifest;
 import android.content.Intent;
 
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,10 +13,13 @@ import android.widget.Toast;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.perfectshot.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CAMERA_PERMISSION = 1;
+    private static final int REQUEST_LOCATION_PERMISSION = 2;
 
     User user;
     SharedPreferences sharedPreferences;
@@ -43,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
         createPost = findViewById(R.id.createPost);
         settings = findViewById(R.id.settings);
         sharedPreferences = getSharedPreferences(prefKey, 0);
+
+        getPermissions();
+
         Log.d("Main", "here");
     }
 
@@ -79,6 +87,24 @@ public class MainActivity extends AppCompatActivity {
         if (user!=null)
         i.putExtra("User",user.toString());
         startActivity(i);
+    }
+
+    private void getPermissions(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    REQUEST_CAMERA_PERMISSION);
+        }else{
+            Log.d("permissiontest","camera granted");
+        }
+
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_LOCATION_PERMISSION);
+        }else{
+            Log.d("permissiontest","location granted");
+        }
     }
 
 
